84eee158282a5f9b3f6fee8e1c638b9155d913b3
JX <jx@chuanyueshikongdexingzhedeMacBook-Air.local>
2025-05-12 15:11:30 +0800

    Update gitignore and remove obsolete files

--- pdm-system/frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-///Users/jx/Docs/PDM/projects/pdm_shared/pdm-system/frontend/vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
